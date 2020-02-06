da4653612b774ec08fc99d7739e6f737527f0124
andrewmarin1 <37843544+andrewmarin1@users.noreply.github.com>
2020-02-06 14:10:59 -0800

    Add files via upload

---
+++ lowkey.rb
@@ -0,0 +1,24 @@
+# Stranger Things Main Theme
+use_bpm 160
+use_synth :hoover
+
+live_loop :main_theme do
+  
+  # YOUR TASK: Use arrays to shorten the 16 lines of code inside this live_loop to only 5 lines of code!
+  play :c2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :c3
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+end
